4e77548ad46730b50ea7aa4c01d30de3f92a434a
LudiYoutuber65 <67953605+LudiYoutuber65@users.noreply.github.com>
2022-06-09 16:26:09 +0200

    Update server.lua
    Dodana funkcija da moraš da budeš admin da bi mogao imati pristup AMeniju

--- server/server.lua
+++ server/server.lua
@@ -7,3 +7,18 @@ AddEventHandler('ludijutuber:stvoriVozilo', function(id, ime)
     local igrac = ESX.GetPlayerFromId(id)
     TriggerClientEvent('esx:spawnVehicle', igrac.source, ime)
 end)
+
+RegisterServerEvent("ludi:admin")
+AddEventHandler("ludi:admin", function()
+      local xPlayer = ESX.GetPlayerFromId(source)
+
+      if xPlayer.getGroup() == 'admin' or xPlayer.getGroup() == 'superadmin' then
+          if xPlayer.proveriDuznost() == true then
+            TriggerClientEvent("professr:admin", source)
+          else
+            TriggerClientEvent('okokNotify:Alert', source, "ADMIN-SISTEM", "moras da budeš na duznosti da otvoris ovo", 5000, 'error')
+          end
+      else
+        TriggerClientEvent('okokNotify:Alert', source, "ADMIN-SISTEM", "Nemas permisiju da radis ovo.", 5000, 'error')
+      end
+  end)
