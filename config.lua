8787b63db71242d002f957900a508b9a8f6824af
LudiYoutuber65 <67953605+LudiYoutuber65@users.noreply.github.com>
2022-06-09 16:31:04 +0200

    Delete config.lua

--- config.lua
+++
@@ -1,2 +0,0 @@
-Config                            = {}
-Config.Webhuk 					  = 'https://discord.com/api/webhooks/955804152099926036/cx8r_lbhPfFyU5tvhKGQoaiyL0kda3mnwbG5vn0HgF7CK3NhWQs1zoqzk7dx-Y8OQqRU'
